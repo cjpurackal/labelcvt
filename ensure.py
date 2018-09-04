1230ee106b4e18a2706ace053806d0b61b7b653f
true-source <christeejacobs@gmail.com>
2018-09-04 16:23:08 +0530

    added cat.names in ensure

--- ensure.py
+++ ensure.py
@@ -26,7 +26,7 @@ def ensure(root, ifmt, lfmt):
 
 
 def directory_structure(root):
-	req_dirs = ['images','labelsxml','lablestxt','labelsyolo', 'labelsbbox', 'unlabelled', 'lost', 'yolo_zip']
+	req_dirs = ['images','labelsxml','lablestxt','labelsyolo', 'labelsbbox', 'unlabelled', 'lost', 'yolo_zip','cat.names']
 	dirs = os.listdir(root)
 	if not set(dirs).issubset(req_dirs):
 		return -1
