6bba9782556928cd2bc6ae00fb90286acf51ffa5
Jeffin Francis <francisjeffin07@gmail.com>
2018-09-14 17:43:26 +0530

    visualize for yolo completed

--- plot/visualize.py
+++ plot/visualize.py
@@ -6,7 +6,50 @@ import numpy as np
 from PIL import Image
 import xml.etree.ElementTree as et
 
-def visualize(dataset_dir,file_path):
+def visualizeyolo(dataset_dir,file_path):
+	cat_name=open(file_path,"r")
+	cat_name=cat_name.readlines()
+	for i,p in enumerate(cat_name):
+		cat_name[i]=p.split("\n")[0]
+	for i in os.listdir(os.path.join(dataset_dir,"labelsyolo")):
+		if i in cat_name:
+			for cats in os.listdir(os.path.join(dataset_dir,"labelsyolo",i)):
+				image_name=cats.split(".")[0]+".jpg"
+				lines=open(os.path.join(dataset_dir,"labelsyolo",i,cats),"r")
+				line=lines.readlines()
+				xmin=[]
+				ymin=[]
+				width=[]
+				height=[]
+				image=os.path.join(dataset_dir,"images",i,image_name)
+				w_,h_=Image.open(image).size
+				for data in line:
+					x_norm=float(data.split(" ")[1])
+					y_norm=float(data.split(" ")[2])
+					w_norm=float(data.split(" ")[3])
+					h_norm=float(data.split(" ")[4])
+					xmid=x_norm*w_
+					ymid=y_norm*h_
+					w=w_norm*w_
+					h=h_norm*h_
+					x1=xmid-w/2
+					y1=ymid-h/2
+					xmin.append(x1)
+					ymin.append(y1)
+					width.append(w)
+					height.append(h)
+				fig,ax=plt.subplots(1)
+				im=np.array(Image.open(image),dtype=np.uint8)
+				ax.imshow(im)
+				print(xmin)
+				for o in range(len(xmin)):
+					s=patches.Rectangle((xmin[o],ymin[o]),width[o],height[o],linewidth=1,edgecolor='b',facecolor="none")
+					ax.add_patch(s)
+				plt.show()
+
+
+
+def visualizexml(dataset_dir,file_path):
 	# dataset_dir=sys.argv[1:][0]
 	# file_path='/home/jeffin/Documents/cat.names'
 	cat_name=open(file_path,"r")
